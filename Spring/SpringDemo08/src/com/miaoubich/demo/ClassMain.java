1c6a03a7ace1166b8ca078975151e7c732b53635
Ali Bouzar <maradga85@yahoo.com>
2018-01-26 11:01:23 +0100

    initial commit

---
+++ Spring/SpringDemo08/src/com/miaoubich/demo/ClassMain.java
@@ -0,0 +1,21 @@
+package com.miaoubich.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ClassMain {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("NewFile.xml");
+		((AbstractApplicationContext) context).registerShutdownHook(); //to destroy all the beans after the program finish
+		
+		Restaurant rObj = (Restaurant) context.getBean("restoBean");
+		rObj.greetCustomer();
+		
+		((AbstractApplicationContext) context).close();
+
+	}
+
+}
