901f3a127407667bffbaf17fd72155e3df8492fb
Ali Bouzar <maradga85@yahoo.com>
2018-01-27 13:47:31 +0100

    immit

---
+++ Spring/SpringDemo Tutorial/SpringDemo12/src/com/miaoubich/demo/Restaurant.java
@@ -0,0 +1,14 @@
+package com.miaoubich.demo;
+
+public class Restaurant {
+
+	private String welcomeNote;
+
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
+	}
+	
+	public void greetCustomer(){
+		System.out.println(welcomeNote);
+	}
+}
