709c98710c5ba17802f1ac1d851b72ef84030d98
Ali Bouzar <maradga85@yahoo.com>
2018-01-29 09:50:23 +0100

    commit

---
+++ Maven/maven tutos/MavenDemo/src/test/java/com/miaoubich/MavenDemo/AppTeyt.java
@@ -0,0 +1,13 @@
+package com.miaoubich.MavenDemo;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class AppTeyt extends TestCase {
+
+	@Test
+	public void test(){
+		
+	}
+}
---
+++ Spring/maven-demo/src/test/java/com/miaoubich/AppTest.java
@@ -0,0 +1,14 @@
+package com.miaoubich;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
